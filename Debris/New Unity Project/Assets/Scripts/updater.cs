2fbbf64631a5b9dad956acf0ce7b10e2367ed967
torishabhmehta <32801724+torishabhmehta@users.noreply.github.com>
2019-10-22 11:03:05 +0530

    update game

---
+++ Debris/New Unity Project/Assets/Scripts/updater.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updater : MonoBehaviour
+{
+    public GameObject spaceCraft;
+    public GameObject spaceCraftAnchor;
+    public Rigidbody rbdAnchor;
+    public attributeDefiner attribute;
+    public updateMethods updateMethod;
+
+    void Start()
+    {   
+        spaceCraft = GameObject.Find("SpaceCraft");
+        spaceCraftAnchor = GameObject.Find("SpaceCraftAnchor");
+        rbdAnchor = spaceCraftAnchor.GetComponent<Rigidbody>();
+        attribute = spaceCraft.GetComponent<attributeDefiner>();
+        updateMethod = spaceCraft.GetComponent<updateMethods>();
+    }
+
+    private void OnTriggerEnter(Collider other) {   
+        updateMethod.damage(1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        Vector3 pos = spaceCraft.transform.localPosition;
+        Vector3 r = spaceCraft.transform.position;
+        Vector3 angVelocity = rbdAnchor.angularVelocity;
+        Vector3 velocity = Vector3.Cross(r, angVelocity);
+
+        float orbit = pos.magnitude*attribute.convFactor*1000f;
+        float optimalSpeed  = 19977378.01f/Mathf.Sqrt(orbit);
+        float thrust = Mathf.Abs(Mathf.Pow(attribute.speed*1000,2) - Mathf.Pow(optimalSpeed,2))/(orbit);
+
+        if (attribute.overheat > 0f || attribute.speed > attribute.maxSpeed) {
+            updateMethod.Heat((attribute.speed - attribute.maxSpeed)*attribute.overheatFactor);
+        }
+
+        if (attribute.overheat >= 100f) {
+            updateMethod.damage((int) attribute.overheat- (int)100f);
+        }
+
+        updateMethod.removeFuel(thrust*attribute.fuelFactor);
+
+        attribute.speed = velocity.magnitude*attribute.convFactor;
+    }
+}
---
+++ Debris/New Unity Project/Assets/Target.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 100f;
+    public void TakeDamage (float amt)
+    {
+      
+            Die();
+        
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
